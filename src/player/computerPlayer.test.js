0cf29db02551dae68b314d2d957c4dd97f19ab7a
makaszy <m.a.kaszynski@protonmail.com>
2023-06-14 16:27:11 +0200

    Add ComputerPlayer attack method

--- src/player/computerPlayer.test.js
+++ src/player/computerPlayer.test.js
@@ -1,14 +1,21 @@
 import ComputerPlayer from "./computerPlayer";
 
-test("Computer player returns a number smaller than 100", () => {
-  const player = new ComputerPlayer();
-  let arr = [];
-  function isBelow(value) {
-    return value > 100
-  }
-  for (let i = 0; i< 100; i++) {
+const player = new ComputerPlayer();
+  const arr = [];
+  
+  for (let i = 0; i < 100; i += 1) {
     arr.push(player.attack());
   }
 
+test("Computer player returns a number smaller than 100", () => {
+  function isBelow(value) {
+    return value <= 100;
+  }
   expect(arr.every(isBelow)).toBe(true)
 })
+
+test("Computer player returns numbers that don't repeat", () => {
+  const duplicateNumberArr = arr.filter((element, index) => arr.indexOf(element) !== index);
+  expect(duplicateNumberArr.length).toBe(0)
+})
+
