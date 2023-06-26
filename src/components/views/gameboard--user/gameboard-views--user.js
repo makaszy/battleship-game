7d61d60c5793de6a4d89a0e7aa5351673ddfae87
makaszy <m.a.kaszynski@protonmail.com>
2023-06-26 12:56:20 +0200

    Add placement temp style

--- src/components/views/gameboard--user/gameboard-views--user.js
+++ src/components/views/gameboard--user/gameboard-views--user.js
@@ -1,6 +1,28 @@
 import GameBoardViewUpdater from "../../common/gameboard-view-updater/gameboard-view-updater";
 import { handleComputerAttack } from "../../pub-subs/attack--computer"
 
+class GameBoardUserViewUpdater extends GameBoardViewUpdater {
+ 
+  clearValidityView() {
+    tiles = document.querySelectorAll("gameboard__tile");
+    tiles.forEach(tile => {
+      tile.classList.remove()
+    }
+  }
+ 
+  handlePlacementValidityView(obj) {
+    if (!obj.valid) {
+      obj.coordinates.forEach(coordinate => {
+        const tile = document.querySelector(
+          `.gameboard--${this.string} [data-id="${coordinate}"]`
+        );
+        tile.classList.add()  
+      });
+
+    }
+  }
+}
+
 const user = "user";
 
 const userViewUpdater = new GameBoardViewUpdater(user);
