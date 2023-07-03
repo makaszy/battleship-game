20487d29fe6ea0c0adaef809ef6118a9e15b2907
makaszy <m.a.kaszynski@protonmail.com>
2023-07-03 18:46:36 +0200

    Create logic for computer attacks

--- src/components/common/gameboard/gameboard.js
+++ src/components/common/gameboard/gameboard.js
@@ -1,5 +1,4 @@
 class GameBoard {
-
   constructor(pubSub) {
     this.pubSub = pubSub;
   }
@@ -33,34 +32,45 @@ class GameBoard {
     return false;
   }
 
-    /* Checks if ship would be neighboring a different ship */
-    isNeighboring(coordinates, direction) {
-      let coordinatesAllNeighbors = [];
-      if (direction === "horizontal") {
-        coordinatesAllNeighbors = coordinatesAllNeighbors.concat(coordinates.map(coor => coor + 10), coordinates.map(coor => coor - 10) ); // top and bottom neighbors
-        if (coordinates[0] === 1 || +(String(coordinates[0]).slice(0, -1)) === 1) {
-          coordinatesAllNeighbors.push(coordinates[-1] + 1); // right neighbor
-        } else if (coordinates[-1] % 10 === 0) {
-          coordinatesAllNeighbors.push(coordinates[0] -1); // left neighbor
-        } else {
-          coordinatesAllNeighbors.push(coordinates[coordinates.length -1] + 1, coordinates[0] -1) // left and right neighbors
-        }
+  /* Checks if ship would be neighboring a different ship */
+  isNeighboring(coordinates, direction) {
+    let coordinatesAllNeighbors = [];
+    if (direction === "horizontal") {
+      coordinatesAllNeighbors = coordinatesAllNeighbors.concat(
+        coordinates.map((coor) => coor + 10),
+        coordinates.map((coor) => coor - 10)
+      ); // top and bottom neighbors
+      if (coordinates[0] === 1 || +String(coordinates[0]).slice(0, -1) === 1) {
+        coordinatesAllNeighbors.push(coordinates[-1] + 1); // right neighbor
+      } else if (coordinates[-1] % 10 === 0) {
+        coordinatesAllNeighbors.push(coordinates[0] - 1); // left neighbor
       } else {
-        coordinatesAllNeighbors = coordinatesAllNeighbors.concat(coordinates.map(coor => coor + 1), coordinates.map(coor => coor - 1) ); // left and right neighbors
-        if (coordinates[0] < 11) {
-          coordinatesAllNeighbors.push(coordinates[-1] + 10); // btm neighbor
-        } else if (coordinates[coordinates.length -1] > 90) {
-          coordinatesAllNeighbors.push(coordinates[0] -10); // top neighbor
-        } else {
-          coordinatesAllNeighbors.push(coordinates[coordinates.length -10] + 1, coordinates[0] -10) // top and btm neighbors
-        }
+        coordinatesAllNeighbors.push(
+          coordinates[coordinates.length - 1] + 1,
+          coordinates[0] - 1
+        ); // left and right neighbors
       }
-      if (this.isTaken(coordinatesAllNeighbors)) {
-        return true
-      } 
-      return false;
-      
+    } else {
+      coordinatesAllNeighbors = coordinatesAllNeighbors.concat(
+        coordinates.map((coor) => coor + 1),
+        coordinates.map((coor) => coor - 1)
+      ); // left and right neighbors
+      if (coordinates[0] < 11) {
+        coordinatesAllNeighbors.push(coordinates[-1] + 10); // btm neighbor
+      } else if (coordinates[coordinates.length - 1] > 90) {
+        coordinatesAllNeighbors.push(coordinates[0] - 10); // top neighbor
+      } else {
+        coordinatesAllNeighbors.push(
+          coordinates[coordinates.length - 10] + 1,
+          coordinates[0] - 10
+        ); // top and btm neighbors
+      }
     }
+    if (this.isTaken(coordinatesAllNeighbors)) {
+      return true;
+    }
+    return false;
+  }
 
   /* Checks if the num selected by player has a ship, if hit checks if ship is sunk, if sunk checks if game is over  */
 
@@ -69,8 +79,14 @@ class GameBoard {
       if (this.ships[y].coordinates.includes(+num)) {
         this.ships[y].hit();
         if (this.ships[y].isSunk()) {
-          const obj = {hit: true, sunk: true, tiles: this.ships[y].coordinates }
-          return (this.isOver()) ? this.pubSub.publish({...obj, ...{gameover: true}}) : this.pubSub.publish(obj)
+          const obj = {
+            hit: true,
+            sunk: true,
+            tiles: this.ships[y].coordinates,
+          };
+          return this.isOver()
+            ? this.pubSub.publish({ ...obj, ...{ gameover: true } })
+            : this.pubSub.publish(obj);
         }
         return this.pubSub.publish({ tile: num, hit: true, sunk: false });
       }
@@ -82,11 +98,10 @@ class GameBoard {
 
   /* Called when a ship is sunk, returns A) GAME OVER if all ships are sunk or B) SUNK if there's more ships left */
 
-  isOver = () => { 
+  isOver = () => {
     const check = this.ships.every((ship) => ship.sunk === true);
-    return check
-  } 
-  
+    return check;
+  };
 }
 
 export default GameBoard;
--- src/components/views/player--computer/player--computer.js
+++ src/components/views/player--computer/player--computer.js
@@ -1,8 +1,11 @@
 import Player from "../../common/player/player";
 import getRandomNum from "../../../utils/get-random-num";
-import { computerAttack } from "../../pub-subs/attack--computer";
+import {
+  computerAttack,
+  handleComputerAttack,
+} from "../../pub-subs/attack--computer";
 import { userAttack } from "../../pub-subs/attack--user";
-import * as init from "../../pub-subs/initialize"
+import * as init from "../../pub-subs/initialize";
 
 class ComputerPlayer extends Player {
   constructor(pubSub) {
@@ -10,21 +13,67 @@ class ComputerPlayer extends Player {
     this.pubSub = pubSub;
   }
 
+  foundShip = {found: false, hit: false, coordinates: [], difference: null, endFound: false,}
+
+  wasAttackSuccess = (obj) => {
+    if (obj.sunk) {
+      this.foundShip = {found: false, hit: false, coordinates: [], difference: null, endFound: false,};
+    } else if (obj.hit && this.foundShip.found === false) {
+      this.foundShip.coordinates.push(obj.tile);
+      this.foundShip.hit = true;
+      this.foundShip.found = true;
+    } else if (obj.hit && this.foundShip.found === true) {
+      this.foundShip.hit = true;
+      this.foundShip.coordinates.push(obj.tile);
+      if (this.foundShip.difference === null) {
+        this.foundShip.difference = Math.abs(this.foundShip.coordinates[0] - obj.tile)
+      } 
+    } else if (obj.hit === false && this.foundShip.found === true) {
+      console.log("endFound")
+      this.foundShip.hit = false;
+      this.foundShip.endFound = true;
+    }
+  }
+
   attack = () => {
-    let num = getRandomNum(101);
-    while (!super.isNew(num)) {
+    console.log(this.foundShip)
+    let num;
+    if (this.foundShip.coordinates.length === 1) {
+      num = this.foundShip.coordinates[0]+1 || this.foundShip.coordinates[0]-1 || this.foundShip.coordinates[0] +10 || this.foundShip.coordinates[0] -10;
+      while (!super.isNew(num) || num > 100 || num < 1) {
+        num = this.foundShip.coordinates[0] +1 || this.foundShip.coordinates[0]-1 || this.foundShip.coordinates[0] +10 || this.foundShip.coordinates[0] -10;
+      }
+    } else if (this.foundShip.coordinates.length > 1 && this.foundShip.hit === true) {
+      if (this.foundShip.endFound === false) {
+        num = this.foundShip.coordinates[this.foundShip.coordinates.length -1] + this.foundShip.difference
+        if (num > 100) {
+          num = this.foundShip.coordinates[0] - this.foundShip.difference
+        }
+        
+      } else if (this.foundShip.endFound === true) {
+        num = this.foundShip.coordinates[0] - this.foundShip.difference
+      } 
+
+    } else if (this.foundShip.coordinates.length > 1 && this.foundShip.hit === false) {
+      this.foundShip.endFound = true;
+      num = this.foundShip.coordinates[0] - this.foundShip.difference
+    }
+   if (this.foundShip.found === false) {
       num = getRandomNum(101);
+      while (!super.isNew(num)) {
+        num = getRandomNum(101);
+      }
     }
     super.attackArr = num;
-    this.pubSub.publish(num)
-    return num
-  }
+    this.pubSub.publish(num);
+    return num;
+  };
 }
 
-function initCompPlayer () {
+function initCompPlayer() {
   const computerPlayer = new ComputerPlayer(computerAttack);
   userAttack.subscribe(computerPlayer.attack);
+  handleComputerAttack.subscribe(computerPlayer.wasAttackSuccess);
 }
 
-init.attackStage.subscribe(initCompPlayer)
-
+init.attackStage.subscribe(initCompPlayer);
