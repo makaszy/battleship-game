7969aea55fdce8ad1f29a6ab890601386e9a121f
makaszy <m.a.kaszynski@protonmail.com>
2023-07-06 17:40:02 +0200

    Annotate the computer player attack method

--- src/components/views/player--computer/player--computer.js
+++ src/components/views/player--computer/player--computer.js
@@ -50,100 +50,92 @@ class ComputerPlayer extends Player {
     ) {
       this.foundShip.hit = false;
       this.foundShip.endFound = true;
-      
+
       this.foundShip.end =
         this.foundShip.coordinates[this.foundShip.coordinates.length - 1];
-     
     } else if (obj.hit === false && this.foundShip.found === true) {
       this.foundShip.hit = false;
     }
   };
 
   static randomSideAttack(coordinate) {
-    const sides = [1, 10]; // data difference for vertical sides is 10, and horizontal sides is 1  
-      const operators = [ // array of operators (+, -) which are used to generate a random operator
-        {
-          sign: "+",
-          method (a, b) {
-            return a + b;
-          },
+    const sides = [1, 10]; // data difference for vertical sides is 10, and horizontal sides is 1
+    const operators = [
+      // array of operators (+, -) which are used to generate a random operator
+      {
+        sign: "+",
+        method(a, b) {
+          return a + b;
         },
-        {
-          sign: "-",
-          method (a, b) {
-            return a - b;
-          },
+      },
+      {
+        sign: "-",
+        method(a, b) {
+          return a - b;
         },
-      ];
-      return operators[Math.floor(Math.random() * operators.length)].method(
-        coordinate/* this.foundShip.coordinates[0] */,
-        sides[Math.floor(Math.random() * sides.length)] 
-      ); // generates the data num of a random side (horizontal left = hit coordinate - 1 / vertical bottom = hit coordinate +10 etc.)
+      },
+    ];
+    return operators[Math.floor(Math.random() * operators.length)].method(
+      coordinate /* this.foundShip.coordinates[0] */,
+      sides[Math.floor(Math.random() * sides.length)]
+    ); // generates the data num of a random side (horizontal left = hit coordinate - 1 / vertical bottom = hit coordinate +10 etc.)
   }
 
   attack = () => {
     let num;
-    if (this.foundShip.coordinates.length === 1) { // if a ship was found, but was only hit once, so it is unknown whether its horizontal or vertical
-      num = ComputerPlayer.randomSideAttack(this.foundShip.coordinates[0])
+    /* A) if a ship was found, but was only hit once, so it is unknown whether its horizontal or vertical */
+    if (this.foundShip.coordinates.length === 1) {
+      num = ComputerPlayer.randomSideAttack(this.foundShip.coordinates[0]);
       while (!super.isNew(num) || num > 100 || num < 1) {
-       num = ComputerPlayer.randomSideAttack(this.foundShip.coordinates[0]) // if the generated num was already attacked, or it's too big or too small to be on the board, it generates the num again
+        num = ComputerPlayer.randomSideAttack(this.foundShip.coordinates[0]); // if the generated num was already attacked, or it's too big or too small to be on the board, it generates the num again
       }
+    /* B) if a ship was found, and was hit more than once, with the last attack also being a hit */  
     } else if (
       this.foundShip.coordinates.length > 1 &&
       this.foundShip.hit === true
     ) {
+      /* B)1. if the end of the ship was not found */
       if (this.foundShip.endFound === false) {
-    /*     let newCoor = this.foundShip.coordinates[this.foundShip.coordinates.length - 1];
-        let prevCoor = this.foundShip.coordinates[this.foundShip.coordinates.length - 2];
-        let coorDiff = this.foundShip.difference; */
-        if (
-         /*  newCoor > prevCoor */
-          this.foundShip.coordinates[this.foundShip.coordinates.length - 1] >
-          this.foundShip.coordinates[this.foundShip.coordinates.length - 2] 
-        ) {
-          num =/*  newCoor + coorDiff; */
-            this.foundShip.coordinates[this.foundShip.coordinates.length - 1] +
-            this.foundShip.difference; 
-        } else if (
-         /*  newCoor < prevCoor */
-          
-          this.foundShip.coordinates[this.foundShip.coordinates.length - 1] <
-          this.foundShip.coordinates[this.foundShip.coordinates.length - 2] 
-        ) {
-          /* num = newCoor - coorDiff/*  */
-          num =  this.foundShip.coordinates[this.foundShip.coordinates.length - 1] -
-            this.foundShip.difference; 
+        let newCoor =
+          this.foundShip.coordinates[this.foundShip.coordinates.length - 1];
+        let prevCoor =
+          this.foundShip.coordinates[this.foundShip.coordinates.length - 2];
+        let coorDiff = this.foundShip.difference;
+        if (newCoor > prevCoor) {
+          num = newCoor + coorDiff;
+        } else if (newCoor < prevCoor) {
+          num = newCoor - coorDiff;
         }
-        if (num > 100 || num < 1 || !super.isNew(num)) {
-          // for edge cases, and situations in which the end tile was already attacked
-          this.foundShip.endFound = true; 
-          this.foundShip.end = this.foundShip.coordinates[this.foundShip.coordinates.length - 1];
-            console.log(`foundshipcoor ${this.foundShip.coordinates[this.foundShip.coordinates.length - 1]}`)
-            
-          this.foundShip.coordinates = this.foundShip.coordinates.sort((a,b) => a - b);
-          console.log(`sorted ${this.foundShip.coordinates}`) //issue is sort not smallest to largest but largest to smallest
-          if (this.foundShip.end === this.foundShip.coordinates[0]) {
+        if (num > 100 || num < 1 || !super.isNew(num)) { // for edge cases, and situations in which the end tile was already attacked
+          this.foundShip.endFound = true;
+          this.foundShip.end = newCoor;
+          this.foundShip.coordinates = this.foundShip.coordinates.sort(
+            (a, b) => a - b
+          );
+          if (this.foundShip.end === this.foundShip.coordinates[0]) { 
             num =
               this.foundShip.coordinates[
                 this.foundShip.coordinates.length - 1
-              ] + this.foundShip.difference;
+              ] + coorDiff;
           } else {
-            num = this.foundShip.coordinates[0] - this.foundShip.difference;
-            console.log(`highest number is end ${num}`)
+            num = this.foundShip.coordinates[0] - coorDiff;
           }
         }
+      /* B)2. if the end of the ship was found */  
       } else if (this.foundShip.endFound === true) {
-        /* console.log num */
-
-        this.foundShip.coordinates = this.foundShip.coordinates.sort((a,b) => a - b);
+        const coorDiff = this.foundShip.difference;
+        this.foundShip.coordinates = this.foundShip.coordinates.sort(
+          (a, b) => a - b
+        );
         if (this.foundShip.end === this.foundShip.coordinates[0]) {
           num =
             this.foundShip.coordinates[this.foundShip.coordinates.length - 1] +
-            this.foundShip.difference;
+            coorDiff;
         } else {
-          num = this.foundShip.coordinates[0] - this.foundShip.difference;
+          num = this.foundShip.coordinates[0] - coorDiff;
         }
       }
+    /* C) if a ship was found, and was hit more than once, with the last attack being a miss */  
     } else if (
       this.foundShip.coordinates.length > 1 &&
       this.foundShip.hit === false
@@ -151,7 +143,9 @@ class ComputerPlayer extends Player {
       this.foundShip.endFound = true;
       this.foundShip.end =
         this.foundShip.coordinates[this.foundShip.coordinates.length - 1];
-      this.foundShip.coordinates = this.foundShip.coordinates.sort((a,b) => a - b);
+      this.foundShip.coordinates = this.foundShip.coordinates.sort(
+        (a, b) => a - b
+      );
       if (this.foundShip.end === this.foundShip.coordinates[0]) {
         num =
           this.foundShip.coordinates[this.foundShip.coordinates.length - 1] +
@@ -159,15 +153,15 @@ class ComputerPlayer extends Player {
       } else {
         num = this.foundShip.coordinates[0] - this.foundShip.difference;
       }
-    }
-    if (this.foundShip.found === false) {
+    /* D) ship was not found */  
+    } else {
       num = getRandomNum(101);
-      while (!super.isNew(num) || num < 1 ) {
+      while (!super.isNew(num) || num < 70) {
         num = getRandomNum(101);
       }
     }
+    /* Publish and Add to arr */
     super.attackArr = num;
-
     console.log(`published ${num}`);
     this.pubSub.publish(num);
     return num;
